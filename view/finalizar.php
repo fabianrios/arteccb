2f6d264b6b0343f93895a3e9886641de441f3d06
fabianrios <hola@fabianrios.co>
2013-03-11 13:54:21 -0500

    cambios correo julie

---
+++ view/finalizar.php
@@ -0,0 +1,39 @@
+
+<?php include_once(SITE_VIEW.'header-login.php'); ?>
+
+
+
+<!-- 2.menu-->
+<?php include_once(SITE_VIEW.'menu.php'); ?>
+<!-- 2. End menu -->
+
+<div class="row main-row">	
+	<div class="panel nopadding">
+		<div class="inner-header">
+			<div class="row">
+				<div class="twelve columns">
+					<p>Gracias por participar en la convocatoria del <strong>Pabellón Artecámara en artBO 2013.</strong> En breve, recibirás un email de confirmación. El listado de los artistas seleccionados podrá ser consultado en <a href="http://www.artboonline.com" target="_blank">www.artboonline.com</a> a partir del <strong>5 de Junio de 2013.</strong></p>					
+				</div>
+			</div>
+		</div>
+		<div class="inner-footer">
+			<div class="container">
+				<div class="row">
+					<div class="twelve columns">
+						
+					</div>
+				</div>
+			</div>
+		</div><!--/inner-footer-->
+	</div><!-- END Main: Panel -->
+	<div class="advisory">
+		<span>Recomendamos visualizar en: IE 9.0 - Firefox 10.0 - Safari 5.1 - Chrome 17.0     |     Optimizada 1024 x 768</span>
+		<span><a href="#">Términos y Condiciones</a> del Sitio</span>
+	</div>
+</div>
+	<!-- 3. END Row main -->
+
+<!-- 4. footer -->			
+<?php include_once('footer.php'); ?>
+<!-- 4. End footer -->
+
