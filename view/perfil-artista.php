d8a79d8533834876c6a8f18f66beed8827beb9a0
fabianrios <hola@fabianrios.co>
2013-04-03 15:42:10 -0500

    segunda fase

---
+++ view/perfil-artista.php
@@ -0,0 +1,495 @@
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<!-- <meta name="viewport" content="width=device-width" /> -->
+
+	<title>Artecámara</title>
+  
+	<!-- Included CSS Files -->
+	<link rel="stylesheet" href="stylesheets/app.css">
+	<link rel="stylesheet" href="stylesheets/new.css">
+	<link rel="stylesheet" href="stylesheets/style.css">
+	<link rel="stylesheet" href="stylesheets/foundation-overrides.css">
+	<link rel="stylesheet" href="stylesheets/phase.css">
+	<link rel="stylesheet" href="stylesheets/jquery.mCustomScrollbar.css">
+	
+	
+	
+	<script type="text/javascript" src="//use.typekit.net/fzq1qvs.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+	
+	<script src="javascripts/jquery.min.js"></script>
+	<script src="javascripts/modernizr.foundation.js"></script>
+	<script src="javascripts/foundation.js"></script>
+	<script src="javascripts/app.js"></script>
+	<script src="javascripts/jquery-ui-1.8.18.custom.min.js"></script>
+	<script src="javascripts/validator.js"></script>
+	
+	
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="stylesheets/ie.css">
+	<![endif]-->
+
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+
+
+<div class="encabezado">
+		<!-- header artbo logo -->
+	<div class="row superior-2">	
+		<!-- columns 1/2 -->
+		<div class="four columns">
+				<img class="logo" src="images/sprite_new.png" height="76" width="436" alt="logo" />
+		</div>
+		<!-- END columns 1/2 -->
+		
+		<!-- columns 2/2 -->
+		<div class="eight columns lateralder ">
+			<div class="perfil-data">
+				<div class="perfil second left">
+					<p class="left"><strong>Angelica Gamboa</strong>
+					<a href="#" title="Clic aquí para editar información del artista">Editar perfil</a> | <a href="#" title="Salir">Salir</a> </p>
+                </div>
+			</div>
+		</div>
+		<!-- END columns 2/2 -->
+	</div>
+		<!-- END header artbo logo -->
+</div>	
+	
+<!-- 2. Navigation -->
+<div class="row breadcrumb">
+	<div class="six columns">
+		<a href="#" class="getback"><span><img src="images/smallarrow.png" alt="" width="17" height="16" /></span>INDICE DE GALERÍAS</a>
+	</div>
+	<div class="six columns">
+		<ul class="main-menu no-bullet">
+			<li><a href="#" class="activo"><span><img src="images/smalldoc.png" alt="" width="12" height="18" /></span>DOCUMENTOS</a></li>
+			<li><a href="#"><span><img src="images/smallfolder.png" alt="" width="17" height="16" /></span>DESCARGAR</a></li>
+			<li><a href="#"><span><img src="images/smalledit.png" alt="" width="17" height="19" /></span>EDITAR</a></li>
+			<li><a href="#" class="activate"><span><img src="images/smallcheck.png" alt="" width="18" height="17" /></span>ACTIVAR</a></li>
+		</ul>
+	</div>
+</div>
+<br />
+
+
+<!-- 2. End menu -->
+
+	<div class="row main-row">	
+		<div class="panel nopadding">
+			<div class="documents-info">
+					<div class="container">
+						<div class="row">
+							<div class="twelve columns">
+								<h3>Documentos</h3>
+								<ul>
+									<li class="right"><img src="images/doc.png" alt="" width="37" height="55" /><span>DOCUMENTO DE IDENTIDAD</span></li>
+								</ul>
+							</div> 
+							<a href="#" class="bigclose"> <img src="images/bigclose.png" alt="" width="30" height="29" /> </a>
+						</div>
+					</div>
+			</div>
+			
+			<div class="profile-resume">
+				<img src="images/galleryprofile.png" alt="" width="597" height="447" />
+				<div class="resum">
+					<h4>Análisis y composición de un sistema difuso</h4>
+					<span class="specification">Video| 1920 x 1080 | 2011</span> 
+					<a href="#" target="_blank" class="website">hugomarin.co/projects/ACSD/</a>
+					<h6>RESEÑA DEL PROYECTO</h6>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc diam, eleifend eu varius semper, cursus nec ipsum. In at arcu eget massa cursus elementum. Suspendisse id tincidunt purus. Pellentesque vel ligula lorem, sit amet porttitor metus. Quisque sit amet nisl ac lorem molestie pellentesque. Etiam odio massa, iaculis at cursus vel, ultrices imperdiet augue. Cras mi lacus, faucibus quis dictum nec, aliquam vitae justo.Pellentesque ultricies lorem sed est pretium adipiscing. Proin tincidunt varius vehicula. Cras venenatis elementum lorem, id lobortis orci fermentum at. Pellentesque tortor sem, iaculis nec fermentum nec, rhoncus et neque. Nullam faucibus velit at diam iaculis in luctus felis dictum. Sed bibendum, felis vel tincidunt euismod, mauris purus fermentum lorem, a consequat lorem massa commodo ligula. Vivamus commodo tristique neque ullamcorper luctus. Phasellus dapibus venenatis mi vitae adipiscing. Morbi in scelerisque turpis. In semper accumsan velit, vitae laoreet nulla placerat et. Integer sagittis, magna et semper bibendum, mi turpis fermentum libero, a mollis quam sapien vel neque. Sed ullamcorper enim quis dolor eleifend vitae gravida enim sagittis.
+					Aliquam orci leo, facilisis vel facilisis sed, feugiat vitae nibh. Integer dapibus, diam sed sollicitudin vulputate, purus est auctor metus, in lobortis lectus dui eu diam. Praesent sit amet leo vel massa varius blandit. Aenean adipiscing consectetur sodales. Sed ultricies sem eu libero dignissim imperdiet. Fusce nisi ligula, vestibulum quis sollicitudin vitae, elementum ac purus. Pellentesque tempor lacinia justo ac dignissim. Curabitur ac magna elit, tincidunt rutrum massa. Nam turpis justo, blandit sed dignissim tristique, blandit mattis risus. Donec a enim non massa rutrum lobortis sit amet ac lectus. Donec eros diam, eleifend eget elementum nec, iaculis et urna. Ut ac egestas tortor. Maecenas bibendum posuere auctor. Aliquam nec lacus id justo vehicula facilisis sit amet sed urna. Etiam ac eros nulla, a scelerisque neque. Morbi nec purus ut orci venenatis vehicula.
+					Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pulvinar erat eu sem ultrices vitae pharetra ante pulvinar. Nam mattis sapien eget diam elementum semper ac id turpis. Nullam felis velit, egestas eu gravida volutpat, malesuada sit amet diam. Praesent convallis tincidunt velit, suscipit tincidunt est viverra non. Curabitur a nibh arcu. Ut semper dapibus ornare. Mauris venenatis ultricies purus at porttitor. Proin varius interdum dolor, ac imperdiet urna semper vel. Curabitur ut urna sed nulla euismod semper. Morbi pellentesque orci a odio volutpat consectetur. Integer sit amet leo ut nibh fringilla mollis.
+					Phasellus et magna ullamcorper leo ultrices scelerisque. Aliquam pharetra ultrices suscipit. Sed in viverra tortor. In sed purus ante, at dignissim augue. Mauris eu leo eros. Vestibulum vulputate nibh tincidunt dolor fermentum porttitor. Cras egestas, lectus vel dignissim aliquam, purus augue accumsan diam, eget dapibus orci lorem eget nibh. Vivamus sit amet dui et mi eleifend aliquam sit amet ut sapien. Nunc condimentum gravida nisl vel gravida. Donec egestas accumsan turpis, vel fermentum orci commodo quis.
+					Nam elit risus, aliquam quis rutrum eget, fermentum malesuada erat. Mauris at lacus eu odio accumsan varius a quis mi. Phasellus iaculis</p>
+				</div>
+			</div>
+			
+			<div class="specific art">
+				<div class="container">
+					<div class="row">
+						<div class="three columns">
+							<h3>Hugo Marín</h3>
+							<ul>
+								<li>
+									<h6>CORREO ELECTRONICO</h6>
+									<span>hm@hugomarin.com</span>
+								</li>
+							</ul>
+						</div>
+						<div class="nine columns">
+							<ul>
+								<li>
+									<h6>CIUDAD DE RESIDENCIA</h6>
+									<span>Bogotá</span>
+								</li>
+								<li>
+									<h6>LUGAR DE NACIMIENTO</h6>
+									<span>Bogotá</span>
+								</li>
+								<li>
+									<h6>IDENTIFICACIÓN</h6>
+									<span>80774028</span>
+								</li>
+								<li>
+									<h6>TELÉFONO</h6>
+									<span>1 2556727</span>
+								</li>
+								<li>
+									<h6>DIRECCIÓN</h6>
+									<span>Cra 13 # 51 39</span>
+								</li>
+								<li>
+									<h6>PAÍS</h6>
+									<span>Colombia</span>
+								</li>
+								<li>
+									<h6>FECHA DE NACIMIENTO</h6>
+									<span>06/25/1981</span>
+								</li>
+								<li>
+									<h6>MÓVIL</h6>
+									<span>1 3166212753</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="folio">
+				<div class="project">
+					<div class="container">
+						<h3>Portafolio</h3>
+						<div class="project-pic">
+							<img src="images/pic.jpg" alt="" width="530" height="331" />
+						</div>
+						<div class="specs">
+							<ul>
+								<li>
+									<h6>Nombre de la Obra</h6>
+									<span>La desdicha</span>
+								</li>
+								<li>
+									<h6>Formato</h6>
+									<span>Video</span>
+								</li>
+								<li>
+									<h6>Dimensiones</h6>
+									<span>1920 x 1080</span>
+								</li>
+								<li>
+									<h6>Url de la obra</h6>
+									<span><a href="#">Ver</a></span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="project">
+					<div class="container">
+						<div class="project-pic">
+							<img src="images/pic2.jpg" alt="" width="530" height="331" />
+						</div>
+						<div class="specs">
+							<ul>
+								<li>
+									<h6>Nombre de la Obra</h6>
+									<span>La desdicha</span>
+								</li>
+								<li>
+									<h6>Formato</h6>
+									<span>Video</span>
+								</li>
+								<li>
+									<h6>Dimensiones</h6>
+									<span>1920 x 1080</span>
+								</li>
+								<li>
+									<h6>Url de la obra</h6>
+									<span><a href="#">Ver</a></span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="project">
+					<div class="container">
+						<div class="project-pic">
+							<img src="images/pic3.jpg" alt="" width="530" height="331" />
+						</div>
+						<div class="specs">
+							<ul>
+								<li>
+									<h6>Nombre de la Obra</h6>
+									<span>La desdicha</span>
+								</li>
+								<li>
+									<h6>Formato</h6>
+									<span>Video</span>
+								</li>
+								<li>
+									<h6>Dimensiones</h6>
+									<span>1920 x 1080</span>
+								</li>
+								<li>
+									<h6>Url de la obra</h6>
+									<span><a href="#">Ver</a></span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="project">
+					<div class="container">
+						<div class="project-pic">
+							<img src="images/pic.jpg" alt="" width="530" height="329" />
+						</div>
+						<div class="specs">
+							<ul>
+								<li>
+									<h6>Nombre de la Obra</h6>
+									<span>La desdicha</span>
+								</li>
+								<li>
+									<h6>Formato</h6>
+									<span>Video</span>
+								</li>
+								<li>
+									<h6>Dimensiones</h6>
+									<span>1920 x 1080</span>
+								</li>
+								<li>
+									<h6>Url de la obra</h6>
+									<span><a href="#">Ver</a></span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="project">
+					<div class="container">
+						<div class="project-pic">
+							<img src="images/pic2.jpg" alt="" width="530" height="329" />
+						</div>
+						<div class="specs">
+							<ul>
+								<li>
+									<h6>Nombre de la Obra</h6>
+									<span>La desdicha</span>
+								</li>
+								<li>
+									<h6>Formato</h6>
+									<span>Video</span>
+								</li>
+								<li>
+									<h6>Dimensiones</h6>
+									<span>1920 x 1080</span>
+								</li>
+								<li>
+									<h6>Url de la obra</h6>
+									<span><a href="#">Ver</a></span>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="project lastspecs">
+					<div class="container">
+						<div class="row">
+							<div class="six columns">
+								<h4>Estudios</h4>
+								<div class="specs">
+									<ul>
+										<li>
+											<h6>Nombre de la Carrera</h6>
+											<span>Artes Plásticas</span>
+										</li>
+										<li>
+											<h6>Institución</h6>
+											<span>UNAL</span>
+										</li>
+										<li class="city">
+											<h6>Ciudad</h6>
+											<span>Bogota</span>
+										</li>
+										<li class="country">
+											<h6>Pais</h6>
+											<span>Colombia</span>
+										</li>
+									</ul>
+									<ul>
+										<li>
+											<h6>Nombre de la Carrera</h6>
+											<span>Master en Artes Plástivas</span>
+										</li>
+										<li>
+											<h6>Institución</h6>
+											<span>UNAL</span>
+										</li>
+										<li class="city">
+											<h6>Ciudad</h6>
+											<span>Bogota</span>
+										</li>
+										<li class="country">
+											<h6>Pais</h6>
+											<span>Colombia</span>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="six columns">
+								<h4>Becas y premios</h4>
+								<div class="specs">
+									<ul>
+										<li>
+											<h6>Beca de Creación</h6>
+										</li>
+										<li>
+											<h6>Premio mejor trabajo de grado</h6>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="exhibitandfairs">
+				<div class="exhibits">
+					<div class="container">
+						<div class="row">
+							<div class="twelve columns">
+								<h3>Exposiciones individuales</h3>
+								<div class="real-expos">
+									<ul>
+										<li><h6>EXPOSICIÓNES</h6></li>
+										<li><h5>Documentales VIII Premio Luis Caballero</h5><span>FGAA, Bogotá, Colombia. 2010</span></li>
+										<li><h5>Documentales VIII Premio Luis Caballero</h5><span>FGAA, Bogotá, Colombia. 2010</span></li>
+										<li><h5>Documentales VIII Premio Luis Caballero</h5><span>FGAA, Bogotá, Colombia. 2010</span></li>
+										<li><h5>Documentales VIII Premio Luis Caballero</h5><span>FGAA, Bogotá, Colombia. 2010</span></li>
+										<li><h5>Documentales VIII Premio Luis Caballero</h5><span>FGAA, Bogotá, Colombia. 2010</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="fairs">
+					<div class="container">
+						<div class="row">
+							<div class="twelve columns">
+								<h3 class="mainh3">Exposiciones Colectivas</h3>
+								<div class="real-fairs">
+									<ul>
+										<li><h6>EXPOSICIÓNES</h6></li>
+										<li><h5>Abstracta Film festival</h5><span>Abstracta, Roma, Italia. 2011</span></li>
+										<li><h5>El gesto en el momento</h5><span>Aurora Pictuare Show, Houston, Estados Unidos, 2011</span></li>
+										<li><h5>El gesto en el momento</h5><span>Aurora Pictuare Show, Houston, Estados Unidos, 2011</span></li>
+										<li><h5>Abstracta Film festival</h5><span>Abstracta, Roma, Italia. 2011</span></li>
+										<li><h5>El gesto en el momento</h5><span>Aurora Pictuare Show, Houston, Estados Unidos, 2011</span></li>
+										<li><h5>El gesto en el momento</h5><span>Aurora Pictuare Show, Houston, Estados Unidos, 2011</span></li>
+										
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+		<div class="activar">
+			<div class="container">
+				<div class="row">
+					<div class="twelve columns">
+						<div class="gallery-name">
+							<span>Desea activar a: <strong>Galería Juana de Aizpuru </strong></span>
+							<a href="#"><span><img src="images/smallcheck.png" alt="" width="18" height="17" /></span>ACTIVAR</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		</div><!-- END Main: Panel -->
+		<div class="footer-menu">
+			<div class="row">
+				<div class="four columns">
+					<div class="left">
+						<a href="#"> < Fabián Ríos</a>
+						<span class="right">ANTERIOR</span>
+					</div>
+				</div>
+				<div class="four columns">
+					<div class="middle text-center"> 
+						<a href="#"><img src="images/smallmenu.png" alt="menu" width="15" height="14" />volver al indice</a>
+					</div>
+				</div>
+				<div class="four columns">
+					<div class="right">
+						<a href="#">Gustavo Gutierrez ></a>
+						<span>SIGUIENTE</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	<!-- 3. END Row main --> 
+	</div>
+	
+	
+		 
+
+<!-- 4. footer -->			
+
+	<script src="javascripts/jquery.min.js"></script>
+	<script src="javascripts/jquery-ui-1.8.18.custom.min.js"></script>
+	<script src="javascripts/modernizr.foundation.js"></script>
+	<script src="javascripts/foundation.js"></script>
+	<script src="javascripts/app.js"></script>	
+    <script src="javascripts/jquery.mousewheel.min.js"></script>
+    <script src="javascripts/jquery.mCustomScrollbar.min.js"></script>
+    
+
+	<!-- Included JS Files -->
+
+	
+	 <script>
+    (function($){
+        $(window).load(function(){
+            $(".specs, .real-expos ul, .real-fairs ul, .this, .resum").mCustomScrollbar({
+            	scrollInertia: 150, 
+            	advanced:{
+			        updateOnContentResize: true
+			    }
+            });
+        });
+    })(jQuery);
+	</script>
+
+</body>
+<!-- End Body -->
+</html>
+
+<!-- 4. End footer -->
+ 
+
+
